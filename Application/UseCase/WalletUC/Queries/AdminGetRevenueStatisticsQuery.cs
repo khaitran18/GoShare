ee719e0574b116a39c7f29ff10c938897c7b2e05
nguyenntse <nguyenntse161785@fpt.edu.vn>
2023-12-11 10:55:40 +0700

    revenue

---
+++ Application/UseCase/WalletUC/Queries/AdminGetRevenueStatisticsQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCase.WalletUC.Queries
+{
+    public class AdminGetRevenueStatisticsQuery : IRequest<RevenueStatisticsDto>
+    {
+    }
+}
