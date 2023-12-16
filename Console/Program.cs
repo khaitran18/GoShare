c4c809a99f81b4b21e9d660d3b6172d82850c53d
Trần Quang Khải <124170490+khaitran18@users.noreply.github.com>
2023-12-16 12:13:16 +0700

    hotfix

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Application.Common.Dtos;
+using Application.Common.Mappers;
 using Application.Common.Utilities;
 using Application.Configuration;
 using Application.Services;
 using Application.Services.Interfaces;
+using Application.UseCase.DriverUC.Commands;
+using Application.UseCase.DriverUC.Handlers;
+using AutoMapper;
 using Domain.DataModels;
 using Domain.Interfaces;
 using Infrastructure;
@@ -22,7 +26,14 @@ partial class Program
     {
         IUnitOfWork _unitOfWork;
         GoShareContext _context;
-        (_unitOfWork,_context) = Configure();
+        IMediator _mediator;
+        (_unitOfWork,_context,_mediator) = Configure();
+        ConfirmPickupPassengerCommand command = new ConfirmPickupPassengerCommand();
+        command.TripId = new Guid("342e3435-66e5-4d65-a6a1-bb8381201362");
+        command.DriverLongitude = (decimal)10.8441264;
+        command.DriverLatitude = (decimal)106.7988646;
+        command.Image = null;
+        await _mediator.Send(command);
         //string phone = "+919191919191";
         //User u = _context.Users.FirstOrDefault(u=>u.Phone.Equals(phone))!;
         //string PasscodeResetToken = OtpUtils.Generate();
@@ -30,6 +41,25 @@ partial class Program
         //u!.Isverify = true;
         //u!.PasscodeResetToken = PasswordHasher.Hash(PasscodeResetToken);
         //u!.PasscodeResetTokenExpiryTime = DateTimeUtilities.GetDateTimeVnNow().AddMinutes(60);
+
+
+    }
+
+
+    private static async Task SendSMSUsingTwilio(string phone)
+    {
+        ITwilioVerification _service = new TwilioVerification(GoShareConfiguration.TwilioAccount);
+        var response = await _service.StartVerificationAsync(phone, "sms");
+        foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(response))
+        {
+            string name = descriptor.Name;
+            object? value = descriptor.GetValue(response);
+            Console.WriteLine("{0}={1}", name, value);
+        }
+    }
+
+    private static async Task CheckTransaction()
+    {
         var response = new List<WalletMonthStatistic>();
         var transactions = new List<Wallettransaction>();
         transactions.Add(new Wallettransaction
@@ -111,19 +141,6 @@ partial class Program
         }
         Console.ReadLine();
     }
-
-
-    private static async Task SendSMSUsingTwilio(string phone)
-    {
-        ITwilioVerification _service = new TwilioVerification(GoShareConfiguration.TwilioAccount);
-        var response = await _service.StartVerificationAsync(phone, "sms");
-        foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(response))
-        {
-            string name = descriptor.Name;
-            object? value = descriptor.GetValue(response);
-            Console.WriteLine("{0}={1}", name, value);
-        }
-    }
     private static async Task VerifyUsingTwilio(string phone,string code)
     {
         try
@@ -144,7 +161,7 @@ partial class Program
 
     }
 
-    private static (IUnitOfWork, GoShareContext) Configure()
+    private static (IUnitOfWork, GoShareContext,IMediator) Configure()
     {
         var builder = new ConfigurationBuilder()
         .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
@@ -154,11 +171,37 @@ partial class Program
         GoShareConfiguration.Initialize(configuration);
         var services = new ServiceCollection();
         services.AddDbContext<GoShareContext>(options => options.UseNpgsql(GoShareConfiguration.ConnectionString("GoShareAzure")));
+        services.AddScoped<IRequestHandler<ConfirmPickupPassengerCommand, TripDto>, ConfirmPickupPassengerHandler>();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+        // Add AutoMapper
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<TestMapper>();
+            cfg.AddProfile<AppfeedbackProfile>();
+            cfg.AddProfile<UserProfile>();
+            cfg.AddProfile<TripProfile>();
+            cfg.AddProfile<CarProfile>();
+            cfg.AddProfile<ChatProfile>();
+            cfg.AddProfile<LocationProfile>();
+            cfg.AddProfile<CartypeProfile>();
+            cfg.AddProfile<DriverdocumentProfile>();
+            cfg.AddProfile<WallettransactionProfile>();
+            cfg.AddProfile<RatingProfile>();
+            cfg.AddProfile<ReportProfile>();
+            cfg.AddProfile<TripImageProfile>();
+        });
+        var mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
+        services.AddSingleton<ISettingService, SettingService>();
         var serviceProvider = services.BuildServiceProvider();
         var _unitOfWork = serviceProvider.GetService<IUnitOfWork>();
         var _context = serviceProvider.GetService<GoShareContext>();
-        return (_unitOfWork!, _context!);
+        var _mediator = serviceProvider.GetService<IMediator>();
+        // Load settings
+        var settingService = serviceProvider.GetRequiredService<ISettingService>();
+        settingService.LoadSettings().Wait();
+        return (_unitOfWork!, _context!,_mediator!);
     }
 
 }
